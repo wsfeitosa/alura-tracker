af33d5bc830554872cde7634eef8fafe1a377a70
wsfeitosa <wsfeitosa@gmail.com>
2023-08-07 19:39:19 -0300

    Separando componentes e adicionando comportamento a os botões

---
+++ src/components/FormularioPrincipal.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulario para criação de uma nova tarefa">
+                <input 
+                    type="text" 
+                    class="input" 
+                    placeholder="Qual tarefa você deseja iniciar?" 
+                    v-model="descricao"
+                />
+            </div>
+            <div class="column">
+                <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Temporizador from './Temporizador.vue';
+
+export default defineComponent({
+    name: 'FormularioPrincipal',
+    components: {
+        Temporizador
+    },
+    data() {
+        return {
+            descricao: ''
+        }
+    },
+    methods: {
+        finalizarTarefa(tempoDecorrido: number): void {
+            console.log(`Tempo da tarefa ${tempoDecorrido}`)
+            console.log(`Descrição da terfa tarefa ${this.descricao}`)
+            this.descricao = ''
+        }
+    }
+})
+</script>
+
+<style></style>
